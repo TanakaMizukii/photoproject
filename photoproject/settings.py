68a6c8a88ce38cc501886563a8ed3faf830ac53e
djangouser <ryusei25110@icloud.com>
2024-06-23 13:52:26 +0900

    'Commit1'

--- photoproject/settings.py
+++ photoproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-l*3)fhzwjmqvc#hn7f3#v(fk15p48)r=2)h&jhznzo)%+q%tdo"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ryusei.pythonanywhere.com']
 
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
