41067ee507aa90a6582a57c49cba5fa96a7d9533
TanakaMizukii <ryusei25110@icloud.com>
2024-06-26 23:52:43 +0900

    Update settings.py

--- photoproject/settings.py
+++ photoproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-l*3)fhzwjmqvc#hn7f3#v(fk15p48)r=2)h&jhznzo)%+q%tdo"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['ryusei.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
